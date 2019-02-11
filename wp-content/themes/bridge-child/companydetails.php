1b544d4871bd17249a405aabb719287f11737b0a
SrujanaSiddigari <siddigarisru@gmail.com>
2019-02-11 15:39:58 +0530

    myprf

--- wp-content/themes/bridge-child/companydetails.php
+++ wp-content/themes/bridge-child/companydetails.php
@@ -5,9 +5,19 @@ Template Name:Company details
 get_header();
 ?>
 <table>
+    <thead>
     <tr>
-        <th>Companyname</th>
-        <th></th>
+       <th>Companyname</th>
+        <th>Primary admin_name</th>
+        <th>Priamry admin_email</th>
+        <th>Country</th>
+        <th>State</th>
+        <th>City</th>
+</tr>
+</thead>
+<tbody>
+    
+</tbody>
 </table>
 <?php
 get_footer();
